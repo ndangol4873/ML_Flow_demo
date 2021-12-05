d8a5afc172ca2f32d93133e535c7adfef97c8f5a
Naresh <nareshkadambini@gmail.com>
2021-12-04 21:25:14 -0500

    adding gitignore

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ML_Flow_demo
 Working with the ML_Flow_Liabrary
+
+# Important Commands
+
+mlflow.set_tracking_uri(remote_server_uri)
+
+mlflow.set_experiment(mlflow_config["experiment_name"]
+
+## mlflow server command -
+
+mlflow server \
+--backend-store-uri sqlite:///mlflow.db \
+--default-artifact-root ./artifacts \
+--host 0.0.0.0 -p 1234
--- ml_app.py
+++ ml_app.py
@@ -55,7 +55,11 @@ if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    with mlflow.start_run():
+    mlflow.set_tracking_uri("http://0.0.0.0:1234")
+
+    mlflow.set_experiment("mlflow_demo")
+
+    with mlflow.start_run(run_name="Lr_Model"):
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
